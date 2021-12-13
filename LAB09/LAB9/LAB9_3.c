9570501a17916ab43ea6771f417ac7462549b3e9
britko <britko@naver.com>
2021-12-13 17:43:51 +0900

    [+]LAB9

--- LAB09/LAB9/LAB9_3.c
+++ LAB09/LAB9/LAB9_3.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 
 void multiples(int num, int count);
+
 int main(void){
 	
 	int num;
 	
 	printf("Enter the number: ");
 	scanf("%d", &num);
+	
 	while(num > 0){
 		multiples(num, 10);
 		printf("Enter the number: ");
 		scanf("%d", &num);
 	}
+	
 	return 0;
 }
 
